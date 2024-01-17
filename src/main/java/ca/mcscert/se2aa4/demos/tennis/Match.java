a5cf67f10cf8736386d1b3f61d4fc2417443fd6a
Kailash Sri <kailash.sri@outlook.com>
2024-01-17 16:22:44 -0500

    minimal & viable product (assuming P1 always win)

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
@@ -1,14 +1,25 @@
 package ca.mcscert.se2aa4.demos.tennis;
 
 public class Match {
-    private String p1_name = "P1";
-    private String p2_name = "P2";
+    private String P1_NAME = "P1";
+    private String P2_NAME = "P2";
 
     public Match(Integer p1Strength, Integer p2Strength) {
     }
 
     public String play() {
-        return "NO WINNER YET....";
+        ScoreSystem scorer = new ScoreSystem();
+        while (!scorer.isEnded()) {
+            String who = decideGameWinner();
+            scorer.score(who);
+        }
+        return scorer.winner()
+                .orElseThrow(() -> new IllegalStateException("No Winner!"));
+    }
+
+    private String decideGameWinner() {
+        System.out.println("Winning this game: " + P1_NAME);
+        return P1_NAME;
     }
 
 }
