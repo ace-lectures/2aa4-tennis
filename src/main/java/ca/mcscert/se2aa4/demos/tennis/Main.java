73002be956eaec8782fb5e6bdfafa9b40784e58d
Kailash Sri <kailash.sri@outlook.com>
2024-01-17 16:07:34 -0500

    demo1

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Main.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Main.java
@@ -6,29 +6,51 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
     public static int PLAYER1_STRENGTH;
     public static int PLAYER2_STRENGTH;
 
-    public static void main(String[] args) {
-        System.out.println("** Starting Tennis Counter Assistant");
-        System.out.println("**** Reading Command-Line Arguments");
+    protected static final Logger logger = LogManager.getLogger();
+
+    private record Configuration(Integer p1Strength, Integer p2Strength) {
+        Configuration {
+            if (p1Strength < 0 || p1Strength > 100)
+                throw new IllegalArgumentException("P1's Strength not in [0,100]: " + p1Strength);
+            if (p2Strength < 0 || p2Strength > 100)
+                throw new IllegalArgumentException("P2's Strength not in [0,100]: " + p2Strength);
+        }
+    }
+
+    private static Configuration configure(String[] commandLineArguments) throws ParseException {
         Options options = new Options();
         options.addOption("p1", true, "Strength of Player 1 in [0,100]");
         options.addOption("p2", true, "Strength of Player 2 in [0,100]");
         CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, commandLineArguments);
+        Integer p1 = Integer.parseInt(cmd.getOptionValue("p1", "50"));
+        Integer p2 = Integer.parseInt(cmd.getOptionValue("p2", "50"));
+        return new Configuration(p1, p2);
+    }
+
+    public static void main(String[] args) {
+        logger.info("** Starting Tennis Counter Assistant");
+        logger.info("**** Reading Command-Line Arguments");
         try {
-            CommandLine cmd = parser.parse(options, args);
-            PLAYER1_STRENGTH = Integer.parseInt(cmd.getOptionValue("p1","50"));
-            System.out.println("****** P1's Strength is " + PLAYER1_STRENGTH+"/100");
-            PLAYER2_STRENGTH = Integer.parseInt(cmd.getOptionValue("p2","50"));
-            System.out.println("****** P2's Strength is " + PLAYER2_STRENGTH+"/100");
-        } catch (ParseException pe) {
-            System.err.println("An error has occurred");
+            Configuration config = configure(args);
+            System.out.println(config);
+
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            logger.error("An error has occurred: " + e);
+            System.exit(1);
         }
-        System.out.println("**** Starting game");
-        System.out.println("** TODO...");
-        System.out.println("** Closing Tennis Counter Assistant");
+        logger.info("**** Starting game");
+        logger.debug("** TODO...");
+        logger.info("** Closing Tennis Counter Assistant");
+
     }
 }
